7bfb5aca70bd5e53d969bef8663bb811ba802d45
candiceywu <candice.ying.wu@gmail.com>
2021-09-14 14:50:55 -0700

    updating notes page

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,54 +1,83 @@
+const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const {
   readFromFile,
   readAndAppend,
   writeToFile,
 } = require('../helpers/fsUtils');
-const fs = require('fs');
+// const fs = require('fs');
 const path = require('path');
 const database = require('/db/db');
 
-
-// // GET Route for notes page via url
-// app.get('/notes', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/notes.html'))
-// );
-
-// GET Route for reading and retrieving all the saved notes
-// app.get('/notes', (req, res) => {
-//   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-// });
+// GET route for retrieving all the notes
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
 
-// GET Route for a specific note being clicked on
-// app.get('/:notes_id', (req, res) => {
+// GET Route for a specific tip
+notes.get('/:notes_id', (req, res) => {
+  const tipId = req.params.notes_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((notes) => notes.notes_id === notesId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json('No note with that ID');
+    });
+});
 
+// DELETE Route for a specific note
+notes.delete('/:notes_id', (req, res) => {
+  const notesId = req.params.notes_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all notes except the one with the ID provided in the URL
+      const result = json.filter((notes) => notes.notes_id !== notesId);
 
-// // GET for adding to the notes list
-// app.get('/notes', (req, res) => {
-//   res.json(database);
-// });
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Note ${notesId} has been deleted 🗑️`);
+    });
+});
 
 // POST Route for a new note
-app.post('/notes', (req, res) => {
-  //receive new note, add it to db.json and return the note on the left side of page
+notes.post('/', (req, res) => {
+  console.log(req.body);
 
-})
+  const { noteTitle, notes } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      noteTitle,
+      notes,
+      notes_id: uuidv4(),
+    };
+
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
 
 
 
 
+// // Display notes.html when /notes is accessed
+// app.get('/notes', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/public/notes.html'));
+// })
 
-
-// Display notes.html when /notes is accessed
-app.get ('/notes', (req, res) => {
-res.sendFile(path.join(__dirname, '/public/notes.html'));
-})
-
-// Display index.html for all other routes
-app.get ('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-})
+// // Display index.html for all other routes
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/public/index.html'));
+// })
 
 // Update json file when a note is added 
 
--- Develop/server.js
+++ Develop/server.js
@@ -1,35 +1,35 @@
 const express = require('express');
-const fs = require('fs');
+// const fs = require('fs');
 const path = require('path');
 
 
+
 const PORT = process.env.port || 3001;
+
 const app = express();
 
 // Middleware for parsing JSON and url encoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-//is this needed as a middleware?
 // app.use('/api', api);
 
 //link to static assets
 app.use(express.static('public'));
 
-// GET Route for homepage to load immediately
+// GET Route for homepage 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for notes page via url
+// GET Route for notes page 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // // Wildcard route to direct users to a 404 page (should always be at the end of the GET routes)
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'public/404.html'))
-// );
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/404.html'))
+);
 
 
 //listen port
