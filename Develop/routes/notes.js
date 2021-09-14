909644ab43cdd08e9188155b7b8ebef34a2f9b7a
candiceywu <candice.ying.wu@gmail.com>
2021-09-13 21:55:02 -0700

    removed unneeeded index.js file from routes

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,4 +1,3 @@
-const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const {
   readFromFile,
@@ -7,35 +6,35 @@ const {
 } = require('../helpers/fsUtils');
 const fs = require('fs');
 const path = require('path');
+const database = require('/db/db.js');
 
 
 
 // GET Route for reading and retrieving all the saved notes
-notes.get('/notes', (req, res) => {
+app.get('/notes', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 
 // GET Route for a specific note being clicked on
-notes.get('/:notes_id', (req, res) => {
+app.get('/:notes_id', (req, res) => {
 
 
-// DELETE Route for a specific note (extra credit)
 
 
 // POST Route for a new note
-notes.post('/notes', (req, res) => {
+app.post('/notes', (req, res) => {
   res.
 })
 
 
 // Display notes.html when /notes is accessed
-notes.get ('/notes', (req, res) => {
+app.get ('/notes', (req, res) => {
 res.
 })
 
 // Display index.html for all other routes
-notes.get ('*', (req, res) => {
+app.get ('*', (req, res) => {
   res.
 })
 
